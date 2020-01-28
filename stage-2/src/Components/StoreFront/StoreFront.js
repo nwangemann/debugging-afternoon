ad316b1c08969fc33eec85e09886d1a8784578b9
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-01-28 14:26:10 -0700

    finishes second

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import './StoreFront.css';
 
 class StoreFront extends Component {
+  constructor(props){
+    super(props)
+
+  }
+
   render() {
+    console.log(this.props)
     let productDisplay = this.props.products.map((element, index) => {
       return (
         <div className="product-container" key={index}>
@@ -16,6 +22,7 @@ class StoreFront extends Component {
     })
     return (
       <div className="storefront-container">
+        {productDisplay}
       </div>
     )
   }
